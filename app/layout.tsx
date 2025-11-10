b6bd6650824b454a50a734ae49301fe5fa5ef87d
Francis <28francis.junior@gmail.com>
2025-11-10 19:48:13 -0300

    fix: update layout component for improved styling and structure; remove unused SVG files

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,18 @@ import Header from "@/components/Header";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"], // pesos que tu quer usar
-  variable: "--font-montserrat", // opcional, pra usar com Tailwind
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-montserrat",
 });
 
 export const metadata: Metadata = {
   title: "Francis Dev",
   description: "Software Developer",
+  icons: {
+    icon: "/star-rock.svg", // <-- define aqui o ícone da aba
+    shortcut: "/star-rock.svg",
+    apple: "/star-rock.svg",
+  },
 };
 
 export default function RootLayout({
@@ -22,10 +27,10 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body
-        className={`${montserrat.variable} ${montserrat.variable} antialiased`}
+        className={`${montserrat.variable} antialiased bg-zinc-950 text-zinc-100`}
       >
-      <Header />
-        {children}
+        <Header />
+        <main>{children}</main>
       </body>
     </html>
   );
